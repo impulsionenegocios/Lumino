c19620b2d44158b97723f5332a967b20fc713999
samuel <impulsionenegocios@gmail.com>
2025-06-15 22:31:58 -0300

    naõ lembro

--- apps/frontend/src/components/headers/mainHeader.vue
+++ apps/frontend/src/components/headers/mainHeader.vue
@@ -62,7 +62,7 @@
     >
       <!-- Sidebar Header: logo + close -->
       <div class="flex items-center justify-between px-6 py-4">
-<img  class="h-[60px]" :src="mainData?.logo_escura" alt="Logo" />
+            <img  class="h-[60px]" :src="mainData?.logo_escura" alt="Logo" />
         <button 
           @click="closeMenu" 
           aria-label="Fechar menu" 
